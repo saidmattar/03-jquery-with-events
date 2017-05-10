9a330062a9f078c7b60959381ca3b3a15392f5c3
saidmattar <ssabdou@gmail.com>
2017-05-10 14:35:31 -0700

    added filtering features for author selection

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -40,7 +40,18 @@ articleView.handleAuthorFilter = function() {
       // TODO: If the select box was changed to an option that has a value, we need to hide all the articles,
       //       and then show just the ones that match for the author that was selected.
       //       Use an "attribute selector" to find those articles, and fade them in for the reader.
+      var $selection = $(this).val();
 
+      $('section#articles').find('article').each(function(){
+        $(this).hide();
+      });
+
+      $('section#articles').find('article').each(function(){
+        if($(this).data('author') === $selection){
+          console.log($(this));
+          $(this).show(500);
+        }
+      });
     } else {
       // TODO: If the select box was changed to an option that is blank, we should
       //       show all the articles, except the one article we are using as a template.
@@ -84,5 +95,6 @@ articleView.setTeasers = function() {
 
 // TODO: Call all of the above functions, once we are sure the DOM is ready.
 $(document).ready(function() {
-
+  articleView.populateFilters();
+  articleView.handleAuthorFilter();
 })
